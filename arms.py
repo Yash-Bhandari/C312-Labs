8e0f920892a22804c876bda3a8c8baf95febffba
Yash Bhandari <yash@bhandari.dev>
2022-09-29 16:07:08 -0600

    anaylitical inverse kinematics

--- arms.py
+++ arms.py
@@ -1,4 +1,4 @@
-from math import cos, sin, pi, acos, asin, atan2
+from math import cos, sin, pi, acos, asin, atan2, isclose
 import sys
 
 class RoboticArm2DoFSim:
@@ -15,7 +15,7 @@ class RoboticArm2DoFSim:
 		self.j2 = j2
 
 	def location_with_angles(self, theta1, theta2):
-		"""What location is reached with the given angles (degrees)"""
+		"""What location is reached with the given angles"""
 		x_1 = self.j1 * cos(theta1)
 		y_1 = self.j1 * sin(theta1)
 		# print("x_1: {:.3f}, y_1: {:.3f}".format(x_1, y_1), file=sys.stderr)
@@ -25,13 +25,15 @@ class RoboticArm2DoFSim:
 		return x_2, y_2
 
 	def angles_for_location(self, x, y):
-		"""What angles are needed to reach the given location"""
-		theta2 = acos(x**2 + y**2 - self.j1**2 - self.j2**2) / (2 * self.j1 * self.j2)
-		theta1 = asin((self.j1 *sin(theta2)) / (x**2 + y**2)**0.5) - atan2(y, x)
-		print("theta1: {:.3f}, theta2: {:.3f}".format(theta1, theta2))
-		return theta1, theta2
+		"""What angles (radians) are needed to reach the given location"""
+		theta2 = acos((x**2 + y**2 - self.j1**2 - self.j2**2) / (2 * self.j1 * self.j2))
+		theta1 = asin((self.j2 *sin(theta2)) / (x**2 + y**2)**0.5) + atan2(y, x)
+		# print("theta1: {:.3f} rad, theta2: {:.3f} rad".format(theta1, theta2), file=sys.stderr)
+		predx, predy = self.location_with_angles(theta1, -theta2)
+		assert isclose(x, predx, abs_tol=0.01), "x: {:.3f}, predx: {:.3f}".format(x, predx)
+		assert isclose(y, predy, abs_tol=0.01), "y: {:.3f}, predy: {:.3f}".format(y, predy)
+		return theta1, -theta2
 
 if __name__ == "__main__":
 	arm = RoboticArm2DoFSim(1, 1)
-	arm.location_with_angles(0, pi)
 	arm.angles_for_location(1, 0.5)
--- driver.py
+++ driver.py
@@ -10,7 +10,6 @@ import sys
 
 JOINT1_LENGTH = 0.117
 JOINT2_LENGTH = 0.096
-
 class RoboticArm:
 	def __init__(self, j1 = JOINT1_LENGTH, j2 = JOINT2_LENGTH, joint1_port = OUTPUT_A, joint2_port= OUTPUT_B) -> None:
 		"""
@@ -37,6 +36,14 @@ class RoboticArm:
 		theta2 = theta2 * 180 / pi
 		pred_x, pred_y = self.sim.location_with_angles(theta1, theta2)
 		print("Predicted Location: {:.3f}, {:.3f}".format(pred_x, pred_y))
+
+	def go_to_position(self, x, y, method = 'analytical'):
+		"""Move the arm to the given location"""
+		if method == 'analytical':
+			theta1, theta2 = self.sim.angles_for_location(x, y)
+		elif method == 'numerical':
+			theta1, theta2 = 0, 0 # fill in here justin!
+		self.set_angles(theta1 * 180 / pi, theta2 * 180 / pi)
 
 	def get_position(self):
 		return self.sim.location_with_angles(self.joint1.position * pi / 180, self.joint2.position * pi / 180)
@@ -78,9 +85,21 @@ def measure_distance():
 		distance = ((x1-x0)**2 + (y1-y0)**2)**0.5
 		print('Distance: {:.2f}cm'.format(distance*100), file=sys.stderr)
 
+# g3 part a
+def go_to_position():
+	arm = RoboticArm()
+	button = Button()
+	positions = [(0.15, 0.1), (0.15, -0.1), (0, 0.15)]
+	for x, y in positions:
+		print('Trying to go to ({:.2f}, {:.2f})'.format(x, y), file=sys.stderr)
+		arm.go_to_position(x, y)
+		arm.print_status()
+		button.wait_for_bump('enter')
+
 if __name__ == "__main__":
 	# repeated_angle_test()
-	measure_distance()
+	# measure_distance()
+	go_to_position()
 
 	# test moving
 	# arm.print_status()
