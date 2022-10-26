e22d49228b7a590a789cd7daee84e402c902db52
Yash Bhandari <yash@bhandari.dev>
2022-10-26 17:53:02 -0600

    getting code ready for submission

---
+++ line_drawing.py
@@ -0,0 +1,8 @@
+from driver import RoboticArm
+
+
+arm = RoboticArm()
+# arm.draw_line(0.15, 0, 0, 0.15)
+# arm.draw_line(0.1, 0, 0.05, 0.1)
+# arm.draw_line(0.15, 0, -0.1, 0.1)
+arm.draw_line(0.12, 0.15, 0, 0.15)
