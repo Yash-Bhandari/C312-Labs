465a60bd009b6ad6a06b6ca27c868f35a9b9f722
Yash Bhandari <yash@bhandari.dev>
2022-11-24 18:46:04 -0700

    svg parser complete

--- project/svg.py
+++ project/svg.py
@@ -9,53 +9,97 @@ import re
 
 @dataclass
 class Shape:
-	type: Literal['rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon', 'path']
-	params: Dict
+	# type: Literal['rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon', 'path']
+	params: Dict = field(kw_only=True, repr=False)
+	stroke: str 
+	stroke_width: int 
+	fill: str 
+
+	def __init__(self, params: Dict):
+		self.params = params
+		self.stroke = params.get('stroke', 'black')
+		self.stroke_width = params.get('stroke-width', 1)
+		self.fill = params.get('fill', 'transparent')
 
 @dataclass
 class RectSVG(Shape):
-	x: int # top left corner
-	y: int
-	width: int
-	height: int
+	x: float # top left corner
+	y: float
+	width: float
+	height: float
+
+	def __init__(self, params: Dict):
+		super().__init__(params)
+		self.x = float(params['x'])
+		self.y = float(params['y'])
+		self.width = float(params['width'])
+		self.height = float(params['height'])
 
 @dataclass
 class CircleSVG(Shape):
-	center_x: int
-	center_y: int
-	radius: int
+	center_x: float
+	center_y: float
+	radius: float
+
+	def __init__(self, params: Dict):
+		super().__init__(params)
+		self.center_x = float(params['cx'])
+		self.center_y = float(params['cy'])
+		self.radius = float(params['r'])
 
 @dataclass
 class EllipseSVG(Shape):
-	center_x: int
-	center_y: int
-	radius_x: int
-	radius_y: int
+	center_x: float
+	center_y: float
+	radius_x: float
+	radius_y: float
+
+	def __init__(self, params: Dict):
+		super().__init__(params)
+		self.center_x = float(params['cx'])
+		self.center_y = float(params['cy'])
+		self.radius_x = float(params['rx'])
+		self.radius_y = float(params['ry'])
 
 @dataclass
 class LineSVG(Shape):
-	x1: int
-	y1: int
-	x2: int
-	y2: int
+	x1: float
+	y1: float
+	x2: float
+	y2: float
+
+	def __init__(self, params: Dict):
+		super().__init__(params)
+		self.x1 = float(params['x1'])
+		self.y1 = float(params['y1'])
+		self.x2 = float(params['x2'])
+		self.y2 = float(params['y2'])
 
 @dataclass
 class PolyLineSVG(Shape):
-	points: List[Tuple[int]]
+	points: List[Tuple[float, float]]
+
+	def __init__(self, params: Dict):
+		super().__init__(params)
+		self.points = []
+		values = [float(x) for x in params['points'].split()]
+		if len(values) % 2 != 0:
+			raise ValueError('Invalid number of values in polyline points')
+		for i in range(0, len(values), 2):
+			self.points.append((values[i], values[i + 1]))
 
 @dataclass 
 class PolygonSVG(Shape):
-	points: List[Tuple[int]]
+	points: List[Tuple[float, float]]
 
-@dataclass
-class PathSVG(Shape):
-	pass
-
-	def __init__(self, params):
-		r'([a-zA-Z][^a-zA-Z]+)'
-
-		self.description = description
-		self.commands = []
+	def __init__(self, params: Dict):
+		super().__init__(params)
+		self.points = []
+		values = [float(x) for x in params['points'].split()]
+		if len(values) % 2 != 0:
+			raise ValueError('Invalid number of values in polygon points')
+		for i in range(0, len(values), 2):
+			self.points.append((values[i], values[i + 1]))
 
 @dataclass
 class PathCommand:
@@ -65,19 +109,40 @@ class PathCommand:
 		Horizontal = 'H'
 		Vertical = 'V'
 		ClosePath = 'Z' # return to first point in path
+		CubicBezier = 'C'
+		StrungCubicBezier = 'S' # unsupported
+		QuadraticBezier = 'Q'
+		StrungQuadraticBezier = 'T' # unsupported
+
+		def __repr__(self):
+			return str(self).split('.')[-1]
 
 	type: Type
 	relative: bool
-	values: List[int]
+	values: List[float]
 
 	def __init__(self, command: str):
 		# first character is the command type
 		self.type = PathCommand.Type(command[0].upper())
 		self.relative = command[0].islower()
+		value_strings = re.findall(r'\-?\d*\.?\d*', command[1:])
 		# split on spaces and commas
-		breakpoint()
-		values = map(int, re.split(r'[\s,]+', command[1:]))
-		
+		self.values = [float(x) for x in value_strings if x != '']
+
+@dataclass
+class PathSVG(Shape):
+	commands: List['PathCommand'] = field(default_factory=list)
+
+	def __init__(self, params):
+		super().__init__(params)
+		description = params['d']
+		command_strings = re.findall(r'([a-zA-Z][^a-zA-Z]+)', description)
+		self.commands = [PathCommand(x) for x in command_strings]
+
+	def __repr__(self):
+		base = super().__repr__()
+		indent = '\n' + ' ' * 4
+		return base + indent + indent.join(map(str, self.commands))
 
 @dataclass
 class SVG:
@@ -85,30 +150,38 @@ class SVG:
 	height: int
 	shapes: List[Shape] = field(default_factory=list)
 
-	def __str__(self):
-		return f"SVG(width={self.width}, height={self.height})"
-
 	def __repr__(self):
-		return f"SVG(width={self.width}, height={self.height})"
+		indent = '\n' + ' ' * 2
+		shapes = indent + indent.join(map(str, self.shapes))
+		return f'SVG(width={self.width}, height={self.height}, shapes={shapes})'
 
+shape_classes = {
+	'rect': RectSVG,
+	'circle': CircleSVG,
+	'ellipse': EllipseSVG,
+	'line': LineSVG,
+	'polyline': PolyLineSVG,
+	'polygon': PolygonSVG,
+	'path': PathSVG,
+}
+
+def parse_shape(element: ET.Element) -> Shape:
+	# '{http://www.w3.org/2000/svg}rect' -> 'rect'
+	shape_type = element.tag.split('}')[1]
+	shape = shape_classes[shape_type](element.attrib)
+	return shape
 
 def parse_svg(path: str):
 	tree = ET.parse(path)
 	root = tree.getroot()
-	width = int(root.attrib["width"])
-	height = int(root.attrib["height"])
+	width = int(root.attrib.get("width", '200'))
+	height = int(root.attrib.get("height", '200'))
 	svg = SVG(width, height)
 	for child in root:
 		shape = parse_shape(child)
 		svg.shapes.append(shape)
 	return svg
 
-def parse_shape(element: ET.Element) -> Shape:
-	# '{http://www.w3.org/2000/svg}rect' -> 'rect'
-	shape_type = element.tag.split('}')[1]
-	shape = Shape(shape_type, element.attrib)
-	return shape
-
 if __name__ == "__main__":
-	path = sys.argv[1] if len(sys.argv) > 1 else "example.svg"
+	path = sys.argv[1] if len(sys.argv) > 1 else "svg_files/example.svg"
 	svg = parse_svg(path)
--- project/test_svg.py
+++ project/test_svg.py
@@ -1,7 +1,18 @@
-from svg import PathCommand
+from svg import PathCommand, PathSVG
 
 def test_path_command():
 	command = PathCommand('m 1 2,3 4')
 	assert command.type == PathCommand.Type.Move
 	assert command.relative
 	assert command.values == [1, 2, 3, 4]
+
+def test_path_svg():
+	d = "M20,230 Q40,205 50,230 T90,230"
+	path = PathSVG({'d': d})
+	assert len(path.commands) == 3
+	assert path.commands[0].type == PathCommand.Type.Move
+	assert path.commands[0].values == [20, 230]
+	assert path.commands[1].type == PathCommand.Type.QuadraticBezier
+	assert path.commands[1].values == [40, 205, 50, 230]
+	assert path.commands[2].type == PathCommand.Type.StrungQuadraticBezier
+	assert path.commands[2].values == [90, 230]
