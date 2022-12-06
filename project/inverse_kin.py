3ec1a16e466b61d173d4d5c94a4107377f1d1ec4
Yash-Bhandari <yash@bhandari.dev>
2022-12-06 12:35:03 -0700

    working inv kin

--- project/inverse_kin.py
+++ project/inverse_kin.py
@@ -1,25 +1,40 @@
 from kinematics import ForwardKinematics
 import numpy as np
 
-def pose_for_location(kin: ForwardKinematics, start_pose, goal, threshold=0.01):
-	# max_movement = 0.6 # the total movement of all arms must be less than .6 rads per step
+def pose_for_location(kin: ForwardKinematics, start_pose, goal, threshold=0.01, retry_count = 5):
+	max_movement = 0.6 # the total movement of all arms must be less than .6 rads per step
 	pose = start_pose
-	while True:
-		position = kin.getPos(pose)
-		delta_y = goal - position
-		distance = np.linalg.norm(delta_y)
-		if distance < threshold:
-			break
+	jitter_size = 0
+	for attempt in range(retry_count):
+		# pose = start_pose + np.random.uniform(-jitter_size, jitter_size, 6)
+		np.clip(pose, 0, np.pi, out=pose)
+		for iter in range(100):
+			position = kin.getPos(pose)
+			delta_y = goal - position
+			distance = np.linalg.norm(delta_y)
+			if distance < threshold:
+				return pose
 
-		jacobian = estimate_jacobian(kin, pose)
-		# solving for delta_x in J * delta_x = delta_y
-		delta_x = np.linalg.pinv(jacobian) @ delta_y
-		# magnitude = np.linalg.norm(delta_x)
-		# direction = delta_x / magnitude
-		# movement = direction * min(magnitude, max_movement)
-		pose += delta_x
+			jacobian = estimate_jacobian(kin, pose)
+			# solving for delta_x in J * delta_x = delta_y
+			delta_x = np.linalg.pinv(jacobian) @ delta_y
+			# make sure we don't move past the joint limits
+			epsilon = 0.05
+			for i in range(len(delta_x)):
+				if pose[i] + delta_x[i] < epsilon:
+					delta_x[i] = -pose[i] + epsilon
+				elif pose[i] + delta_x[i] > np.pi - epsilon:
+					delta_x[i] = np.pi - pose[i] - epsilon
+
+			magnitude = np.linalg.norm(delta_x)
+			direction = delta_x / magnitude
+			movement = direction * min(magnitude, max_movement)
+			pose += movement
+
+		pose = np.random.uniform(0, np.pi, 6)
+		print(f"Warning: IK did not converge. Randomizing starting pose")
 	return pose
-
+		
 def estimate_jacobian(kin: ForwardKinematics, pose: np.ndarray):
 	y = kin.getPos(pose)
 	jacobian = np.zeros((3, 6), np.float64)
--- project/test_inverse_kin.py
+++ project/test_inverse_kin.py
@@ -2,6 +2,7 @@ from kinematics import ForwardKinematics
 from inverse_kin import pose_for_location
 import numpy as np
 import pytest
+import math
 
 # -- robot parameters --
 JOINTS = [[4.05, 0, 6.75], [0, 0, 9], [-0.85, 0, 2.165],
@@ -12,15 +13,24 @@ ORIGIN = [-8.35, 0, 2.6]
 @pytest.mark.parametrize('goal', [
 	([10, 5, 10]),
 	([15, 8, 1]),
-	([0, 0, 0]),
+	([0, 1, 0]),
 	([0, 5, 10]),
 	([3, 10, 0]),
+	([7, 1, 2]),
+	([4, -5, 5]),
+	([2, -5, 1]),
+	([5, -10, 3]),
+	([2, -5, 10]),
+	([-3, 14.5, -5])
 	])
 def test_pose_for_location(goal):
 	kin = ForwardKinematics(JOINTS, ORIGIN)
-	start_pose = np.array([0, 0, 0, 0, 0, 0], np.float64)
+	start_pose = np.array([.2, .1, .5, .2, 1, 0.5], np.float64)
+	# start_pose = np.random.uniform(0, math.pi, 6)
 	goal = np.array(goal)
 	pose = pose_for_location(kin, start_pose, goal)
 	estimated_position = kin.getPos(pose)
-	assert np.allclose(estimated_position, goal, atol=0.01)
+	assert np.allclose(estimated_position, goal, atol=0.1)
+	assert min(pose) >= 0
+	assert max(pose) <= math.pi
 	
