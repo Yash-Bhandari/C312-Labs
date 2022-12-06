9bcd746315cc5c3b70cc73b879c5f88ccf46f4fa
Yash-Bhandari <yash@bhandari.dev>
2022-12-06 14:27:13 -0700

    driver sleep

--- project/driver.py
+++ project/driver.py
@@ -1,12 +1,15 @@
 from robot import Robot
 import numpy as np
+import time
 
 def terminal():
 	arm = Robot()
+	arm.print_status()
 	while True:
 		command = input("Enter command as x y z: ")
 		loc = np.array([float(x) for x in command.split()])
 		arm.move2location(loc)
+		time.sleep(3)
 		arm.print_status()
 
 if __name__ == "__main__":
